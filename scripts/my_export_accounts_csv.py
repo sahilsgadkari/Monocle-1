74c89e32f019d6af2f16ebfc6674882ae6fe32c8
RobTwoThree <rsayson31@gmail.com>
2017-08-28 20:10:19 -0700

    Correction.

--- scripts/my_export_accounts_csv.py
+++ scripts/my_export_accounts_csv.py
@@ -67,7 +67,7 @@ if invalid:
     with invalid_file.open('a') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         if write_header:
-            writer.writerow(('username', 'password', 'provider', 'level', 'created', 'last used'))
+            writer.writerow(('username', 'password', 'provider', 'model', 'iOS', 'id'))
         for account in invalid:
             row = [account['username'], account['password'], account['provider']]
             writer.writerow(row)
